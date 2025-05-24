ab8d475e0eb83d3f7005a5debd4302b8003c1ba2
Syed Umer Mujahid Hassni <109479332+ft-umer@users.noreply.github.com>
2025-05-24 09:37:06 -0700

    backend

--- src/pages/waste/ListWastePage.tsx
+++ src/pages/waste/ListWastePage.tsx
@@ -18,7 +18,7 @@ const ListWastePage: React.FC = () => {
   useEffect(() => {
     const fetchWasteItems = async () => {
       try {
-        const response = await fetch('https://backend-e-waste-management.vercel.app/api/waste');
+        const response = await fetch('https://backend-e-waste.vercel.app/api/waste');
         const data = await response.json();
         setWasteItems(data);
         setLoading(false);
